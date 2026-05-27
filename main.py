bcd55fca025e4285472636601946aa6bc66eb9e0
sparklejosh <sparklejosh.gxd@gmail.com>
2026-05-27 08:39:04 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
 from fastapi import FastAPI, UploadFile, File
-from faster_whisper import WhisperModel
-import shutil
+from fastapi.middleware.cors import CORSMiddleware
+import whisper
+import tempfile
 import os
 
 app = FastAPI()
 
-model = WhisperModel("base", device="cpu", compute_type="int8")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+model = whisper.load_model("base")
 
 @app.get("/")
 def home():
-    return {"status": "Sermon AI Running on Render"}
+    return {"status": "Sermon AI Running"}
 
 @app.post("/transcribe")
 async def transcribe(file: UploadFile = File(...)):
-    path = "audio.mp3"
+    try:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_audio:
+            temp_audio.write(await file.read())
+            temp_audio_path = temp_audio.name
 
-    with open(path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
+        result = model.transcribe(temp_audio_path)
 
-    segments, info = model.transcribe(path)
+        os.remove(temp_audio_path)
 
-    text = ""
-    for s in segments:
-        text += s.text + " "
+        return {
+            "transcript": result["text"]
+        }
 
-    return {"transcript": text}
+    except Exception as e:
+        return {
+            "error": str(e)
+        }
